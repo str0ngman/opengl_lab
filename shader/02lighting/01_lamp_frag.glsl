cd7e5115624636e9c6dbb60bef2d066b3fc829ff
strongman <116357931@qq.com>
2016-11-16 16:19:43 +0800

    Lighting: add a basic lamp and a basic container

---
+++ shader/02lighting/01_lamp_frag.glsl
@@ -0,0 +1,11 @@
+//------------------------------
+//01_lamp_frag.glsl
+//------------------------------
+
+#version 330 core
+out vec4 color;
+
+void main()
+{
+    color = vec4(1.0f); // Set alle 4 vector values to 1.0f
+}
